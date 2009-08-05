e6c5e4b414c9c629b4bedbad965f7bc4fa941784
dlandau <dlandau@90502d85-042b-4514-9db1-d3be52136ceb>
2009-08-05 18:43:07 +0000

    Merging in Quickmail module.
    git-svn-id: file:///var/svn/moodle/branches/1.9.5-LAE1.0/Release@97 90502d85-042b-4514-9db1-d3be52136ceb

---
+++ blocks/quickmail/lang/he_utf8/block_quickmail.php
@@ -0,0 +1,41 @@
+<?php // $Id: block_quickmail.php,v 1.1 2007/08/30 18:46:09 mark-nielsen Exp $
+/**
+ * Language entries for Quickmail
+ *
+ * @author Mark Nielsen
+ * @version $Id: block_quickmail.php,v 1.1 2007/08/30 18:46:09 mark-nielsen Exp $
+ * @package quickmail
+ * @Translated by Miki Alliel  
+ **/
+
+$string['action'] = 'פעולה';
+$string['areyousure'] = 'האם אתה בטוח כי ברצונך למחוק את כל הדוא\"לים מההיסטוריה?';
+$string['attachment'] = 'קובץ מצורף';
+$string['attachmentalt'] = 'הוסף קובץ מצורף לדוא\"ל שלך';
+$string['attachmenterror'] = 'קובץ מצורף זה שגוי! הקובץ לא קיים: <strong>$a</strong>';
+$string['attachmentoptional'] = 'קובץ מצורף (אופציונלי)';
+$string['blockname'] = 'דוא\"ל מהיר';
+$string['check'] = 'בחר הכל';
+$string['clearhistory'] = 'נקה היסטוריה';
+$string['compose'] = 'כתוב דוא\"ל';
+$string['date'] = 'תאריך';
+$string['delete'] = 'מחק';
+$string['deletefail'] = 'מחיקה נכשלה';
+$string['deletesuccess'] = 'מחיקה הצליחה';
+$string['email'] = 'דוא\"ל';
+$string['emailfail'] = 'שגיאה בדוא\"ל';
+$string['emailfailerror'] = 'הדוא\"ל לא נשלח בהצלחה למשתמשים הבאים בגלל ש...';
+$string['emailstop'] = 'כתובות דוא\"ל מנוטרלות';
+$string['history'] = 'היסטוריה';
+$string['messageerror'] = 'חייבת להיות הודעה';
+$string['nogroupmembers'] = 'אין חברי קבוצות';
+$string['notingroup'] = 'לא נמצא בקבוצה';
+$string['quickmail:cansend'] = 'יכול לשלוח דוא\"לים דרך תוכנת quickmail';
+$string['sendemail'] = 'שלח דואר';
+$string['subjecterror'] = 'חייב להיות נושא!';
+$string['successfulemail'] = 'הדוא\"ל נשלח בהצלחה!';
+$string['to'] = 'אל';
+$string['toerror'] = 'חייב לבחור נמענים עבור הדוא\"ל';
+$string['uncheck'] = 'בטל בחירה לכל';
+
+?>
