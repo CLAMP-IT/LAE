a0e10433626d5ed2cf41a7c456ead033c760318d
dlandau <dlandau@90502d85-042b-4514-9db1-d3be52136ceb>
2009-08-05 18:42:25 +0000

    Merging in FilteredCourseList module.
    git-svn-id: file:///var/svn/moodle/branches/1.9.5-LAE1.0/Release@96 90502d85-042b-4514-9db1-d3be52136ceb

---
+++ blocks/filtered_course_list/lang/en_utf8/block_filtered_course_list.php
@@ -0,0 +1,16 @@
+<?PHP 
+
+$string['allcourses']               = 'Admin user sees all courses';
+$string['blockname']                = 'Current Courses List';
+$string['configadminview']          = 'What should the admin see in the course list block?';
+$string['confighideallcourseslink'] = 'Hide \"All courses\" link at the bottom of the block. <br>Link hiding does not affects Admin\'s view';
+$string['owncourses']               = 'Admin user sees own courses';
+$string['configfiltertype']         = 'Select the type of course filter you\'d like to use.';
+$string['filterterm']               = 'By Term';
+$string['filtercategories']         = 'By Categories';
+$string['filtercustom']             = 'Custom';
+$string['configtermcurrent']        = 'Enter the string that designates the current term. <br>This must be part of the course shortname.';
+$string['configtermfuture']         = 'Enter the string that designates the next term. <br>This must be part of the course shortname.'; 
+$string['configcategories']         = 'Select the category you want to display as current.'; 
+
+?>
---
+++ blocks/filtered_course_list/readme.txt
@@ -0,0 +1,25 @@
+README for Simple File Upload
+developed by the Collaborative Liberal Arts Moodle Project ( clamp-it.org )
+version: 1.0 2009-07-29
+
+Installation: 
+
+Unzip files into your Moodle blocks directory. This will create a folder called filtered_course_list. 
+
+Go to the admin Notifications page to complete installation.
+
+
+Configuration: 
+
+To configure the block, go to Site Administration > Modules > Blocks > Current Courses List
+
+On this page, you can choose to filter by Terms or by Categories. 
+
+Choose "terms" if you organize courses by including a certain string within the course shortname (e.g. BIO101-01S09 = the S09 at the end indicates the term). 
+
+Choose "categories" if you organize your courses into categories by term.  
+
+
+Plans for Future Development: 
+
+We will be adding the ability to choose multiple categories or substrings to be shown as "current" or "future" for both categories and terms.  
